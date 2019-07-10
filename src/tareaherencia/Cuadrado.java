24554e7e9e65fada227defcf66aa019d81e5d468
David Mejia <dfmejia0298@gmail.com>
2019-07-09 22:47:21 -0600

    Quinta tarea de POO

---
+++ src/tareaherencia/Cuadrado.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tareaherencia;
+
+/**
+ *
+ * @author fam
+ */
+public class Cuadrado extends Formas {
+    public Cuadrado(){
+        
+        setdibujar("Cuadrado");
+        setareacuadrado(4);
+    }
+}
