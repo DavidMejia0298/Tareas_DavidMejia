ab65437724cd188c66b79b2c3f90b4ec78f852f3
David Mejia <dfmejia0298@gmail.com>
2019-07-09 22:43:07 -0600

    Quinta tarea de POO

---
+++ src/tareaherencia/Circulo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tareaherencia;
+
+/**
+ *
+ * @author fam
+ */
+public class Circulo extends Formas {
+    
+    public Circulo(){ 
+        setdibujar("Circulo");
+        setradio(5);
+    }
+}
